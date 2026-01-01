506393bddda286936181ef16933726ef32ba1341
hayago <ironcat110@gmail.com>
2026-01-02 08:27:05 +0900

    sagemaker integration

---
+++ scripts/inference.py
@@ -0,0 +1,24 @@
+import sentencepiece as spm
+from transformers import LlamaForCausalLM
+import torch
+
+sp = spm.SentencePieceProcessor(model_file="data/sentencepiece/out.model")
+
+input_texts = [
+    "今日は朝から雨が降っていて、",
+    "静かな図書館の中で、私は一冊の古い本を開いた。",
+    "夜が更けてきた。\n街の明かりは、\n",
+    "この町では、季節が変わるたびに、人々の暮らし方や考え方が少しずつ変化していくのだが、",
+    "遠くで鐘の音が鳴り始めたとき、",
+]
+
+for text in input_texts:
+    input_ids = sp.encode_as_ids(text)
+    input_ids = [sp.bos_id()] + input_ids
+    input_ids = torch.tensor(input_ids).unsqueeze(0)
+
+    model = LlamaForCausalLM.from_pretrained("Llama-3.2-1B-Japanese")
+    outputs = model.generate(input_ids)
+    generated_ids = outputs[0].tolist()
+    generated_text = sp.decode(generated_ids)
+    print(generated_text)
---
+++ scripts/train_model.py
@@ -0,0 +1,137 @@
+import argparse
+import sentencepiece as spm
+from accelerate import Accelerator
+from datasets import load_dataset
+from transformers import LlamaForCausalLM, AutoConfig
+from transformers import DefaultDataCollator
+from transformers import Trainer, TrainingArguments
+
+
+# Load the sentencepiece model
+sp = spm.SentencePieceProcessor(model_file="sentencepiece/out.model")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--resume", action="store_true")
+    return parser.parse_args()
+
+
+def preprocess(examples) -> dict:
+    # Encode the text into ids and add BOS and EOS
+    all_ids = []
+    for ids in sp.encode_as_ids(examples["text"]):
+        all_ids.extend([sp.bos_id()] + ids + [sp.eos_id()])
+
+    # Pack the all_ids into chunks of context_length
+    context_length = 2048
+    total_length = (len(all_ids) // context_length) * context_length
+    chunks = [
+        all_ids[i : i + context_length] for i in range(0, total_length, context_length)
+    ]
+
+    return {
+        "input_ids": chunks,
+        "labels": chunks.copy(),  # labels are the same as input_ids for causal language modeling
+    }
+
+
+def prepare_dataset():
+    # Load the dataset
+    dataset_name = "hayago/cohere-wikipedia-22-12-ja-text"
+    wikipedia_dataset = load_dataset(dataset_name)
+
+    train_dataset = wikipedia_dataset["train"]
+
+    # Use half of the validation dataset for evaluation
+    val_dataset_size = len(wikipedia_dataset["validation"])
+    val_dataset = wikipedia_dataset["validation"].select(range(val_dataset_size // 2))
+
+    # Preprocess the dataset
+    packed_dataset_train = train_dataset.map(
+        preprocess, batched=True, remove_columns=["text"]
+    )
+    packed_dataset_val = val_dataset.map(
+        preprocess, batched=True, remove_columns=["text"]
+    )
+
+    return packed_dataset_train, packed_dataset_val
+
+
+def log_training_info(num_samples, training_args):
+    batch_size = training_args.per_device_train_batch_size
+    gradient_accumulation_steps = training_args.gradient_accumulation_steps
+    num_epochs = training_args.num_train_epochs
+    steps_per_epoch = num_samples // (batch_size * gradient_accumulation_steps)
+    total_steps = steps_per_epoch * num_epochs
+    print(f"Number of samples: {num_samples}")
+    print(f"Batch size: {batch_size}")
+    print(f"Gradient accumulation steps: {gradient_accumulation_steps}")
+    print(f"Number of epochs: {num_epochs}")
+    print(f"Steps per epoch: {steps_per_epoch}")
+    print(f"Total steps: {total_steps}")
+
+
+def main():
+    args = parse_args()
+
+    # Prepare the dataset with caching for distributed training
+    acc = Accelerator()
+    if acc.is_main_process:
+        # Main process creates dataset cache
+        packed_dataset_train, packed_dataset_val = prepare_dataset()
+    acc.wait_for_everyone()
+    if not acc.is_main_process:
+        # Other processes load from cache
+        packed_dataset_train, packed_dataset_val = prepare_dataset()
+
+    # Initialize or load the model
+    if args.resume:
+        model = LlamaForCausalLM.from_pretrained("Veloce-1B")
+    else:
+        model_config = AutoConfig.from_pretrained(
+            "meta-llama/Llama-3.2-1B",
+            vocab_size=sp.vocab_size(),
+            bos_token_id=sp.bos_id(),
+            eos_token_id=sp.eos_id(),
+        )
+        model = LlamaForCausalLM(model_config)
+
+    # Training arguments
+    training_args = TrainingArguments(
+        output_dir="Veloce-1B",
+        per_device_train_batch_size=1,
+        per_device_eval_batch_size=1,
+        gradient_accumulation_steps=64,
+        warmup_steps=4,  # -> 40
+        lr_scheduler_type="cosine",
+        weight_decay=0.1,
+        fp16=True,
+        logging_strategy="steps",
+        logging_steps=2,  # -> 20
+        eval_strategy="steps",
+        logging_first_step=True,
+        eval_steps=10,  # -> 100
+        num_train_epochs=5,  # -> 1
+        learning_rate=5e-4,
+        save_steps=10,  # -> 100
+        report_to="wandb",
+        push_to_hub=True,
+        hub_strategy="all_checkpoints",
+    )
+
+    log_training_info(len(packed_dataset_train), training_args)
+
+    # Train the model
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=packed_dataset_train,
+        eval_dataset=packed_dataset_val,
+        data_collator=DefaultDataCollator(),
+    )
+    trainer.train(resume_from_checkpoint=args.resume)
+
+
+if __name__ == "__main__":
+    main()
