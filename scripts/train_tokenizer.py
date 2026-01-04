3f08b55fc9f0869f637ae1a98b5c1f5941f37147
hayago <ironcat110@gmail.com>
2026-01-04 09:06:23 +0900

    add user defined symbols

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -1,10 +1,10 @@
 import io
 import os
 import sentencepiece as spm
-from datasets import load_from_disk
+from datasets import load_dataset
 
 # Load the dataset and create an iterator over the text
-wikipedia_dataset = load_from_disk("data/wikipedia_dataset")
+wikipedia_dataset = load_dataset("hayago/cohere-wikipedia-22-12-ja-text")
 train_dataset = wikipedia_dataset["train"]
 sentence_it = (sample["text"] for sample in train_dataset)
 
@@ -13,10 +13,34 @@ model = io.BytesIO()
 
 # Train the tokenizer
 sp = spm.SentencePieceTrainer.train(
-    sentence_iterator=sentence_it, vocab_size=32000, model_writer=model
+    sentence_iterator=sentence_it,
+    vocab_size=32000,
+    model_writer=model,
+    user_defined_symbols=[
+        "<system>",
+        "<user>",
+        "<assistant>",
+        "<sep>",
+        "<reserved_0>",
+        "<reserved_1>",
+        "<reserved_2>",
+        "<reserved_3>",
+        "<reserved_4>",
+        "<reserved_5>",
+        "<reserved_6>",
+        "<reserved_7>",
+        "<reserved_8>",
+        "<reserved_9>",
+        "<reserved_10>",
+        "<reserved_11>",
+        "<reserved_12>",
+        "<reserved_13>",
+        "<reserved_14>",
+        "<reserved_15>",
+    ],
 )
 
 # Serialize the model as file.
-os.makedirs("data/sentencepiece", exist_ok=True)
-with open("data/sentencepiece/out.model", "wb") as f:
+os.makedirs("sentencepiece", exist_ok=True)
+with open("sentencepiece/out.model", "wb") as f:
     f.write(model.getvalue())
